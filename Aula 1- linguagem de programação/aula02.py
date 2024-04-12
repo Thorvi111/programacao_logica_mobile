26a7d81dc1b86c3237ef0dc674d52f0c1dbed7b2
Vitor Hugo Andrade de Aguiar <158524396+Thorvi111@users.noreply.github.com>
2024-04-11 21:49:10 -0300

    Update, Python
    Fiz a atividade do dia 11-4, Parabéns Gizelle-

---
+++ Aula 1- linguagem de programação/aula02.py
@@ -0,0 +1,19 @@
+print("ola mundo, finalmente quebrei a maldição")
+
+#print ele vai exibir tudo oq esta dentro do parenteses e aspas 
+
+print("olá bem vindo ao nosso app!")
+
+nome = input("Qual é o seu nome ?:")
+
+idade = input("Qual sua idade ?:")
+
+gosto = input("Qual é o seu hobbie ?:")
+
+#input é servir alguma coisa
+
+print(f"Olá {nome}, você tem {idade} anos, e {gosto} é seu hobbie favorito ")
+
+#print("Seu nome é David, sua idade é 25 anos. Gosta de tocar bateria, na igreja!")
+
+
